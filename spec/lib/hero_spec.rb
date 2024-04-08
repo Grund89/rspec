e5a4de99219cd45735603964b2ddec40bf34eb33
Douglas Grund <douglasgrund@yahoo.com.br>
2024-04-08 16:22:18 -0300

    add new code

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -1,6 +1,16 @@
+# require 'spec_helper'
+# require 'hero'
+
+# describe Hero do
+#   it 'has a sword'
+# end
+
 require 'spec_helper'
 require 'hero'
 
 describe Hero do
-  it 'has a sword'
+  it 'has a sword' do
+    hero = Hero.new
+    expect(hero.has_sword?).to be true # Aqui estamos verificando se o herói tem uma espada
+  end
 end
