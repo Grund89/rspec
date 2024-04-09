31a829af1acf9c9e263ba0cf330d854e948088f7
Douglas Grund <douglasgrund@yahoo.com.br>
2024-04-09 15:04:47 -0300

    git clone

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -1,28 +1,9 @@
-# require 'spec_helper'
-# require 'hero'
+require 'spec_helper'
+require 'hero'
 
-# describe Hero do
-#   it 'has a sword'
-# end
-
-# require 'spec_helper'
-# require 'hero'
-
-# describe Hero do
-#   it 'has a sword' do
-#     hero = Hero.new
-#     expect(hero.has_sword?).to be true # Aqui estamos verificando se o herói tem uma espada
-#   end
-# end
-
-class Hero
-  attr_reader :weapon
-
-  def initialize(weapon)
-    @weapon = weapon
-  end
-
-  def has_sword?
-    @weapon == "sword"
+describe Hero do
+  it 'possui uma espada' do
+    hero = Hero.new
+    expect(hero.has_sword?).to be true
   end
 end
